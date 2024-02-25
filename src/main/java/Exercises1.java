1f5e92f451f7c169fd221f360482fdce89cc85da
kouroshMojdehi <k.rashidimojdehi@gmail.com>
2024-02-25 20:02:06 +0330

    Exercise 1 completed

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -1,10 +1,25 @@
+
 public class Exercises1 {
     /*
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
-        // TODO
-        return 0;
+
+        // usual answer
+//        long fact =1;
+//        if(n==0){
+//            return 1;
+//        }
+//        for(int i=1 ; i<= n ; i++){
+//
+//            fact *= i;
+//        }
+//        ...........................
+        // Recursive
+        if(n==1 || n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 
 
@@ -13,8 +28,24 @@ public class Exercises1 {
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
+        // Recursive
+//        if (n == 1 || n == 2) {
+//            return 1;
+//        }
+//        return fibonacci(n - 1) + fibonacci(n - 2);
+
+        //non_recursive answer
+        long lastFibo = 1;
+        long currentFibo =1;
+        if(n==1 || n==2){
+            return currentFibo;
+        }
+        for(int i=3 ; i<=n ; i++){
+            long temp = currentFibo;
+            currentFibo =lastFibo +currentFibo ;
+            lastFibo =  temp ;
+        }
+        return currentFibo;
     }
 
 
@@ -30,13 +61,21 @@ public class Exercises1 {
 
      */
     public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+        char [][] starTriangle  = new char [rows][];
+        for(int i =0 ; i < rows ; i++ ){
+            starTriangle [i] = new char[i +1];
+            for(int j=0 ; j< i+1 ; j++){
+                starTriangle [i][j] ='*';
+            }
+        }
+
+        return starTriangle;
     }
 
 
     public static void main(String[] args) {
         // test your code here!
+
     }
 
 }
